7cb69cdd10bfac3d8c2f79b8d52bf1a6493a09c8
Tiago Torres <tmptorres@outlook.com>
2025-04-09 18:27:18 +0200

    fix webcam access

--- src/detection/frame_source/webCamSource.py
+++ src/detection/frame_source/webCamSource.py
@@ -4,25 +4,21 @@ import numpy as np
 from src.detection.frame_source.imageRectification import ImageRectification
 
 class WebCamSource:
-    def __init__(self, webcam_id=0,
-                 save_stream_path='/home/tiago/repos/fetz_as/src/detection/data/testSeqxxx.mp4'):
+    def __init__(self, options):
         # Open webcam
-        self.cap = cv2.VideoCapture(webcam_id)
+        self.cap = cv2.VideoCapture(options['webcam_id'])
         if not self.cap.isOpened():
             raise Exception("Error: Could not open webcam.")
 
-        # Create rectification LUT
-        self.rectification = ImageRectification()
-
         # Optionally save webcam stream to file
-        if save_stream_path is None:
-            self.out = None
-        else:
+        if 'webcam_save_stream_path' in options.keys():
             fourcc = cv2.VideoWriter_fourcc(*'MP4V')
-            self.out = cv2.VideoWriter(filename=save_stream_path,
+            self.out = cv2.VideoWriter(filename=options['webcam_save_stream_path'],
                                        fourcc=fourcc,
                                        fps=self.cap.get(cv2.CAP_PROP_FPS),
                                        frameSize=np.array([self.cap.get(cv2.CAP_PROP_FRAME_WIDTH), self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT)]).astype(int))
+        else:
+            self.out = None
 
     def get_frame(self):
         ret, frame = self.cap.read()
@@ -32,8 +28,6 @@ class WebCamSource:
 
         if self.out is not None:
             self.out.write(frame)
-
-        frame = self.rectification.rectify(frame)
 
         # camera_properties = {
         #     'width': cv2.CAP_PROP_FRAME_WIDTH,
@@ -50,8 +44,6 @@ class WebCamSource:
         #     print(f"{prop}: {self.cap.get(value)}")
         # self.cap.set(cv2.CAP_PROP_EXPOSURE, 3)
         # self.cap.set(cv2.CAP_PROP_FPS, 60)
-
-        # Save frame to video file
 
         return frame
 
